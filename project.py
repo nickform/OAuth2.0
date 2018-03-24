b25f75e438aaf7f063392905b9c0e301fd59cd66
Nick Form <1095240+nickform@users.noreply.github.com>
2018-03-24 14:40:01 +0000

    Lesson 2, Step 2: Anti-CSRF token

--- project.py
+++ project.py
@@ -6,6 +6,9 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+# Lesson 2, Step 2 
+from flask import session as login_session
+import random, string
 
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
@@ -14,6 +17,13 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Lesson 2, Step 2
+@app.route('/login')
+def showLogin():
+  state = ''.join(random.choice(string.ascii_uppercase + string.
+    digits) for x in range(32))
+  login_session['state'] = state
+  return "The current session state is %s" %login_session['state']
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
